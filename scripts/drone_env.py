c641895e7120b29bd1d3caa99b99896d86adb518
Ecem Isildar <ecemisildar@gmail.com>
2024-04-30 18:32:39 +0200

    ros1 code

--- scripts/drone_env.py
+++ scripts/drone_env.py
@@ -1,254 +1,298 @@
 #!/usr/bin/env python3
 import rospy
 import gym
+from gym import Env
+import math
 import cv2
+from cv_bridge import CvBridge
+import random
+
 import imutils
 import tensorflow as tf
 import os
 import math
 import numpy as np
 import time
-import stable_baselines3 as sb3
+
 import tensorflow as tf
 
 
-from cv_bridge import CvBridge
-from gym import spaces
 from std_srvs.srv import Empty
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import Image, CompressedImage
+from sensor_msgs.msg import Image
 from air_drone.msg import Position, MotorSpeed
-from stable_baselines3.common.noise import NormalActionNoise
 
-from target_points import positions_matrix
+
 from target_points import spawn
+from target_points import check_balls_in_environment
 from target_points import delete_object_service
-import sensor_msgs.point_cloud2 as pc2
 
-class DroneEnv(gym.Env):
+
+VERBOSE = False
+class DroneEnv(Env):
     def __init__(self):
-        super(DroneEnv, self).__init__()
-		
         rospy.init_node('drone_env')
-        self.bridge = CvBridge()
-        self.closest_distance = float('inf')
-        self.width = 640
-        self.height = 480
-        self.distance = 100
 
-        self.action_space = spaces.Box(low=np.array([-1.0, -1.0, -1.0, -1.0]),
-                                       high=np.array([1.0, 1.0, 1.0, 1.0]),
-                                       dtype=np.float32)  
-                      
-        # self.observation_space = spaces.Box(low=0, high=255,
-		# 									shape=(1, ), 
-        #                                     dtype=np.uint8) # height and width
-        self.observation_space = spaces.Box(low=0, high=255, 
-                                            shape=(self.height, self.width), 
-                                            dtype=np.uint8)
+
+
+        self.width = 80
+        self.height = 60
         
-        self.action_noise = NormalActionNoise(mean=np.zeros(self.action_space.shape), 
-                                              sigma=0.3 * np.ones(self.action_space.shape))
+        self.bridge = CvBridge()
+        # self.min_non_zero_distance = 100
+        
+        self.collected_targets = []
+        self.depth_image = []
+        self.detection_reward = 0
+        
+
+        self.last_position = None
+        self.start_time = None
+        self.check = False
+        self.green_penalty = 0
+        self.previous_green_pixels = 0
 
 
+        # self.action_space = Box(low=np.array([-1.0, -1.0]),
+        #                         high=np.array([1.0, 1.0]), shape=(2,),
+        #                         dtype=np.float32)  
+    
+        self.action_space = gym.spaces.Box(low=-1, high=1, shape=(2,), dtype=np.float32)
+        self.observation_space = gym.spaces.Box(low=0, high=255, shape=(60,80,1), dtype=np.uint8)
+        # self.observation_space = gym.spaces.Box(low=-10, high=10, shape=(6,), dtype=float)
+
+
+        # self.observation_space = gym.spaces.Dict({
+        #     "depth_map": Box(low=0, high=255,shape=(60,80,1), dtype=np.uint8),
+        #     "current_pose": Box(low=-10, high=10, shape=(6,), dtype=float)})
+        
+       
         # Publishers, subscribers and services
-        self.current_pose_sub = rospy.Subscriber("pose", Position, self.position_callback)  
-        self.z_speed = rospy.Publisher("motor_speed_cmd", MotorSpeed, queue_size=1) # motor speed for z direction
+        self.current_pose_sub = rospy.Subscriber("pose", Position, self.position_callback)    
+        self.image_sub = rospy.Subscriber("/camera/depth/image_raw", Image, self.camera_callback) # depth camera
         self.speed_motors_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)  # motor speed 
         self.reset_simulation_service = rospy.ServiceProxy('/gazebo/reset_simulation', Empty) # resets gazebo  
-        self.image_sub = rospy.Subscriber("/depth_camera/depth/image_raw", Image, self.camera_callback) # depth camera
-        self.point_sub = rospy.Subscriber("/depth_camera/depth/points", pc2.PointCloud2, self.point_callback)
+        self.image_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.image_callback)
 
-    def point_callback(self, data):
-        points = np.array(list(pc2.read_points(data, field_names=("x", "y", "z"), skip_nans=True)))
-        # Calculate the distance between the camera and the closest point in the point cloud
-        self.distance = np.min(np.sqrt(points[:, 0]**2 + points[:, 1]**2 + points[:, 2]**2))
-        # rospy.loginfo("Distance: %f", self.distance)
 
-    def camera_callback(self, image_msg):
-        self.depth_image = self.bridge.imgmsg_to_cv2(image_msg, desired_encoding='passthrough')
-        # Replace NaN values with zeros in the copied array
-        self.depth_image = np.nan_to_num(self.depth_image)
-
-        if np.any(np.isnan(self.depth_image)):
-            print("***********nan values in depth_image************")    
+    def image_callback(self, data):
         
-    def position_callback(self, pos_msg):
-        self.pose_x, self.pose_y, self.pose_z = pos_msg.x, pos_msg.y, pos_msg.z
-        self.pitch, self.roll = pos_msg.pitch, pos_msg.roll
+        cv_image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+    
+        # Convert BGR to HSV
+        hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
-        self.current_position = [self.pose_x, self.pose_y, self.pose_z]
+        # Define range of green color in HSV
+        lower_green = np.array([40, 40, 40])
+        upper_green = np.array([80, 255, 255])
 
-    def object_name_finder(self):
-                
-        object_name = None
-        target_1 = np.take(positions_matrix, 0, axis=0)
-        target_2 = np.take(positions_matrix, 1, axis=0)
-        target_3 = np.take(positions_matrix, 2, axis=0)
+        # Threshold the HSV image to get only green colors
+        mask = cv2.inRange(hsv_image, lower_green, upper_green)
 
-        self.dist_target_1 = np.linalg.norm(self.current_position - target_1)
-        self.dist_target_2 = np.linalg.norm(self.current_position - target_2)
-        self.dist_target_3 = np.linalg.norm(self.current_position - target_3)
+        num_green_pixels = cv2.countNonZero(mask)
+    
+        self.green_pixels = num_green_pixels 
 
-        if  self.dist_target_1 < 0.5:
-            if self.flag_1 == False:
-                object_name = 'green_ball_1'
-                delete_object_service(object_name)
-                # rospy.loginfo(f"object name in finder func:{object_name}")
-                self.collected_target_points += 1
-                self.flag_1 = True
-                
-        if  self.dist_target_2 < 0.5: 
-            if self.flag_2 == False:
-                object_name = 'green_ball_2' 
-                delete_object_service(object_name)
-                # rospy.loginfo(f"object name in finder func:{object_name}")
-                self.collected_target_points += 1
-                self.flag_2 = True
-                
-
-        if self.dist_target_3 < 0.5: 
-            if self.flag_3 == False:
-                object_name = 'green_ball_3'
-                delete_object_service(object_name)
-                # rospy.loginfo(f"object name in finder func:{object_name}")
-                self.collected_target_points += 1
-                self.flag_3 = True
-
-        return object_name 
-
-    def check_collision(self):
-
-        collision_count = 0
-        if collision_count == 0 and self.distance < 1.0 or abs(self.roll) > math.radians(15) or abs(self.pitch) > math.radians(15):
-
-            delete_object_service('green_ball_1')
-            delete_object_service('green_ball_2')
-            delete_object_service('green_ball_3')
-            delete_object_service('green_ball_4')
-
-            self.done = True
-           #reset func
-            self.flag_1 = False
-            self.flag_2 = False
-            self.flag_3 = False
-
-            self.linear_x = 0.0
-            self.dist_target_1 = 10000
-            self.dist_target_2 = 10000
-            self.dist_target_3 = 10000
-
-            self.collected_target_points = 0
-            self.pose_x, self.pose_y, self.pose_z = 0.0, 0.0, 0.0
-            self.current_position = [self.pose_x, self.pose_y, self.pose_z]
-
-            self.spawn_last_target = False
-
-            self.width = 640
-            self.height = 480
-
-            self.time_taken = 0
-            self.reward = 0
-
-            self.depth_image = np.zeros((self.height, self.width)) 
-            self.observation = self.depth_image
-  
-            spawn(1)
-            spawn(2)
-            spawn(3)
-            self.reset_simulation_service() 
-            
-
-            # self.collected_target_points = 0
-
-            # self.time_taken = 0
-            # self.reward = 0 
-
-            collision_count += 1
-            return self.done
-        return self.done    
-            
-    def take_action(self, action):
-        self.object_name_finder()
-        self.vel_cmd = Twist()
-        self.z_cmd = MotorSpeed()
-
-        self.linear_x = 2.5*(action[0]+1)/2 #scaling (-1,1) to (0,1) because velocity should be positive
-        linear_y = action[1]*2.5
-        angular_z = action[2]*2.5
-        z_vel = action[3]*125
-
-        self.vel_cmd.linear.x = self.linear_x
-        self.vel_cmd.linear.y = linear_y
-        self.vel_cmd.angular.z = angular_z
-        self.speed_motors_pub.publish(self.vel_cmd)
-
-        self.z_cmd.name = ['propeller1', 'propeller2', 'propeller3', 'propeller4', 'propeller5', 'propeller6']
-        self.z_cmd.velocity = [z_vel, -z_vel , z_vel, -z_vel, z_vel, -z_vel]
-        self.z_speed.publish(self.z_cmd)
-
-    def step(self, action):
-        self.take_action(action)
-        self.check_collision()
-
-        if self.collected_target_points == 3:
-            if self.spawn_last_target == False:
-                spawn(4)
-                self.spawn_last_target = True             
-
-        self.time_taken += 0.01 
-        collecting_reward = 2/(1+math.exp(self.collected_target_points))  
-
-        self.reward = -max(self.linear_x/self.distance,0) - self.time_taken + 100*collecting_reward 
-
-        if self.collected_target_points == 4:
-            self.done = True
-
-        if np.isnan(self.reward):
-            print("***********nan values in reward*************")    
-        
-        # print(f"Reward: {self.reward}")
-        
-        self.observation = self.depth_image
-        info = {}
-
-        return self.observation, self.reward, self.done, info
-	
-    def reset(self):
-        print("Reset function called")
-        self.done = False
-        self.flag_1 = False
-        self.flag_2 = False
-        self.flag_3 = False
-
-        self.linear_x = 0.0
-        self.dist_target_1 = 10000
-        self.dist_target_2 = 10000
-        self.dist_target_3 = 10000
-
-        self.collected_target_points = 0
-        self.pose_x, self.pose_y, self.pose_z = 0.0, 0.0, 0.0
-        self.current_position = [self.pose_x, self.pose_y, self.pose_z]
-
-        self.spawn_last_target = False
-
-        self.width = 640
-        self.height = 480
-
-        self.time_taken = 0
-        self.reward = 0
-
-        self.depth_image = np.zeros((self.height, self.width)) 
-        self.observation = self.depth_image
-
-        spawn(1)
-        spawn(2)
-        spawn(3)
-
-
-        return self.observation 
+        # Find contours
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
 
-if __name__ == '__main__':   
-    DroneEnv()       
+        # Filter contours based on size and shape
+        min_contour_area = 100
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area > min_contour_area:
+                # Draw bounding box around detected green objects
+                # print("**************GREEN BALL********************")
+                x, y, w, h = cv2.boundingRect(contour)
+                self.ball_coords = (x,y)
+                current_position_xy = tuple(self.current_position[:2]) 
+                self.dist = np.linalg.norm(np.array(current_position_xy) - np.array(self.ball_coords))
+                # print(self.dist)
+                cv2.rectangle(cv_image, (x, y), (x+w, y+h), (0, 255, 0), -1)
+                
+        # Display the result
+        cv2.imshow('Green Ball Detection', cv_image)
+        cv2.waitKey(1)
+
+
+    def camera_callback(self, image_msg):
+        image = self.bridge.imgmsg_to_cv2(image_msg, desired_encoding='passthrough')
+        
+        if image.size > 0:
+            image = np.nan_to_num(image)
+            resized_depth_image = np.expand_dims(image, axis=-1)
+            # Convert resized depth image to float32
+            self.depth_image = np.array(resized_depth_image, dtype=np.float32)
+
+            depth_array = np.array(self.depth_image, dtype=np.float32)
+
+            non_zero_mask = (depth_array != 0)
+            if np.any(non_zero_mask):
+                self.min_non_zero_distance = np.min(depth_array[non_zero_mask])
+  
+
+    def position_callback(self, pos_msg):
+        pose_x, pose_y, pose_z = pos_msg.x, pos_msg.y, pos_msg.z
+        pitch, roll, yaw = pos_msg.pitch, pos_msg.roll, pos_msg.yaw
+
+        self.current_position = [pose_x, pose_y, pose_z]
+        self.current_pose = np.array([pose_x, pose_y, pose_z, pitch, roll, yaw])
+
+        tolerance = 0.2
+
+        if self.last_position is None:
+            self.last_position = self.current_position
+            self.start_time = time.time()
+        else:
+            if np.linalg.norm(np.array(self.last_position) - np.array(self.current_position)) < tolerance:
+                a = time.time() - self.start_time
+                
+                if  a >= 5.0:
+                    print("Drone position has remained stable for 5 seconds.")
+                    self.done = True
+                    
+            else:
+                self.last_position = self.current_position
+                self.start_time = time.time()
+
+
+    def object_name_finder(self):
+
+        object_name = None
+        current_position_xy = self.current_position[:2] 
+
+        distances = {}
+
+        for name, self.pos in self.target_positions.items():
+            if name not in self.collected_targets: # Check if target is not collected
+                distance = np.linalg.norm(current_position_xy - self.pos[:2])
+                distances[name] = distance
+
+
+        if distances:
+            min_target = min(distances, key=distances.get)
+            min_distance = distances[min_target]
+            self.min_distance = min_distance
+            print(self.min_distance)
+
+            if min_distance < 1.0:
+                object_name = f"{min_target}"
+                status = delete_object_service(object_name)
+                if object_name in self.target_positions:
+                    del self.target_positions[object_name]
+
+                if status:
+                    self.collected_target_points += 1
+                    print(f"Collected balls: {self.collected_target_points}")
+
+
+        return object_name
+
+    def check_collision(self):
+        collision_count = 0
+        # print(self.min_non_zero_distance)
+        if collision_count == 0 and self.min_non_zero_distance < 0.7 or abs(self.current_position[0])>8 or abs(self.current_position[1])>8:  
+            print("*******************collision*********************")
+            self.check = True
+            self.done = True
+            # self.reset()
+            collision_count += 1
+
+        return self.done   
+                
+    def take_action(self, action):
+        
+        self.object_name_finder()
+
+
+        linear_x = (action[0]+1)/2
+        angular_z = action[1]
+        
+        vel_cmd = Twist()
+        vel_cmd.linear.x = 5.0*linear_x
+        vel_cmd.angular.z = 10.0*angular_z
+
+        self.speed_motors_pub.publish(vel_cmd)
+
+    def step(self, action):
+        print("Step function called")
+        self.done = False
+        self.take_action(action)
+        self.check_collision() 
+        
+        reward = -1 + 2/(1+np.exp(0.5*self.min_distance/(self.green_pixels+1)))
+        print(f"# Green pxl: {self.green_pixels}")
+        print(f"Reward: {reward}")
+       
+
+        if check_balls_in_environment():
+            pass
+        else:    
+            print("******ALL TARGETS ARE COLLECTED******")
+            self.done = True
+
+           
+        self.observation = self.depth_image
+        # self.observation = {"depth_map": self.depth_image,
+        #                     "current_pose": self.current_pose}
+       
+       
+        # print(self.observation)
+       
+
+        info = {}
+
+        return self.observation, reward, self.done, info
 	
+    def reset(self):
+        
+        print(f"Reset function called")
+       
+        
+        self.last_position = None
+        self.start_time = None
+       
+        self.green_pixels = 0
+        self.collected_target_points = 0
+        self.current_position = [0,0,0]
+        self.detection_reward = 0
+        self.min_distance = np.array([100])
+        self.current_pose = np.array([0,0,0,0,0,0])
+
+        self.spawn_last_target = False
+        self.ball_coords = [0,0]
+
+        image = np.zeros((self.height, self.width)) 
+
+        resized_depth_image = np.expand_dims(image, axis=-1)   
+        depth_image = np.array(resized_depth_image, dtype=np.float32)
+        self.observation = self.depth_image
+        # self.observation = {"depth_map": self.depth_image,
+        #                     "current_pose": self.current_pose}
+        vel_cmd = Twist()
+        vel_cmd.linear.x = 0.0
+        vel_cmd.angular.z = 0.0
+
+        self.speed_motors_pub.publish(vel_cmd)
+       
+
+        delete_object_service('green_ball_1')
+        # delete_object_service('green_ball_2')
+        # delete_object_service('green_ball_3')
+        # delete_object_service('green_ball_4')
+        self.reset_simulation_service()
+        print("simulation started")
+
+        self.target_1 = spawn(1)
+        # self.target_2 = spawn(2)
+        # self.target_3 = spawn(3)
+
+        self.target_positions = {
+            'green_ball_1': self.target_1[:2],
+            # 'green_ball_2': self.target_2[:2],
+            # 'green_ball_3': self.target_3[:2],
+        }
+        
+        return self.observation
+    
--- scripts/target_points.py
+++ scripts/target_points.py
@@ -1,70 +1,66 @@
 #!/usr/bin/env python3
 import numpy as np
 import rospy 
-from gazebo_msgs.srv import DeleteModel, DeleteModelRequest, SpawnModel
+from gazebo_msgs.srv import DeleteModel, DeleteModelRequest, SpawnModel, GetWorldProperties
 from geometry_msgs.msg import Pose
+from gazebo_msgs.msg import ModelStates
 
 
 # rospy.init_node('target_points')
 spawn_model = rospy.ServiceProxy("/gazebo/spawn_sdf_model", SpawnModel)
 delete_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
 
-# random positions for 3 goal points
-# Create a 4x3 matrix with random values between -4 and 4
-positions_matrix = np.random.uniform(1, 5, (4, 3))
-# Set the last item of each row to 1
-for i in range(positions_matrix.shape[0] - 1):
-    positions_matrix[i, -1] = 0.5
-# Ensure the last row remains 0,0,1
-positions_matrix[3, :] = np.array([0, 0, 0.5])
-positions_matrix = np.array(positions_matrix)
-
             
 def spawn(number):
     rospy.wait_for_service("/gazebo/spawn_sdf_model")
+
+    # random positions for 3 goal points
+    global positions_matrix
+    positions_matrix = np.random.uniform(-3, 3, (4, 3))
+    # positions_matrix = np.array([[2.0, 0.0, 1.0],
+    #                     [2.0, 2.0, 1.0],
+    #                     [4.0, 4.0, 1.0]])
+    for i in range(positions_matrix.shape[0]):
+        positions_matrix[i, -1] = 1.0
+
+    positions_matrix = np.array(positions_matrix)
     
-    model_sdf = open("/home/ecem/.gazebo/models/green_ball_1/model.sdf", "r").read()
+    model_sdf = open("/home/ecem/.gazebo/models/green_ball/model.sdf", "r").read()
     model_namespace = ""
     # Set the initial pose
     initial_pose = Pose()
-    if number == 1:
-        print("1st ball spawned")
-        model_name = "green_ball_1"
-        initial_pose.position.x = positions_matrix[0,0]
-        initial_pose.position.y = positions_matrix[0,1]
-        initial_pose.position.z = positions_matrix[0,2]
-        number = 0
-
-    elif number == 2:
-        print("2nd ball spawned")
-        model_name = "green_ball_2"
-        initial_pose.position.x = positions_matrix[1,0]
-        initial_pose.position.y = positions_matrix[1,1]
-        initial_pose.position.z = positions_matrix[1,2]
-        number = 0
-
-    elif number == 3:
-        print("3rd ball spawned")
-        model_name = "green_ball_3"
-        initial_pose.position.x = positions_matrix[2,0]
-        initial_pose.position.y = positions_matrix[2,1]
-        initial_pose.position.z = positions_matrix[2,2]
-        number = 0
-
-    elif number == 4:
-        print("4th ball spawned")
+    if number == 4:
         model_name = "green_ball_4"
-        initial_pose.position.x = positions_matrix[3,0]
-        initial_pose.position.y = positions_matrix[3,1]
-        initial_pose.position.z = positions_matrix[3,2] 
-        number = 0   
+        initial_pose.position.x = 0
+        initial_pose.position.y = 0
+        initial_pose.position.z = 1.0
+
+    else:
+        model_name = f"green_ball_{number}"
+        initial_pose.position.x = positions_matrix[number - 1,0]
+        initial_pose.position.y = positions_matrix[number - 1,1]
+        initial_pose.position.z = positions_matrix[number - 1,2]
+
+
 
     success, status_message = spawn_target_points(model_name, model_sdf, model_namespace, initial_pose)
 
     if success:
         rospy.loginfo(f"Successfully spawned {model_name} in Gazebo!")
-    # else:
-    #     rospy.logerr(f"Failed to spawn {model_name} in Gazebo. Error: {status_message}")
+    else:
+        rospy.logerr(f"Failed to spawn {model_name} in Gazebo. Error: {status_message}")
+
+    return positions_matrix[number - 1] if number != 4 else np.array([0, 0, 1.0])
+  
+def model_states_callback(msg):
+    global spawned_balls
+    spawned_balls = [model_name for model_name in msg.name if "green_ball_" in model_name]
+
+# Function to check if any balls are present in the environment
+def check_balls_in_environment():
+    return len(spawned_balls) > 0
+
+rospy.Subscriber('/gazebo/model_states', ModelStates, model_states_callback)
 
 def spawn_target_points(model_name, model_sdf, model_namespace, initial_pose):
     try:
@@ -74,14 +70,29 @@ def spawn_target_points(model_name, model_sdf, model_namespace, initial_pose):
         return False, f"Service call failed: {str(e)}"
 
 def delete_object_service(object_name):
+    rospy.wait_for_service('/gazebo/get_world_properties')
     rospy.wait_for_service('/gazebo/delete_model')    
   
-    req = DeleteModelRequest()
-    req.model_name = str(object_name)  # Specify the name of the object to delete
-    resp = delete_model(req)
-    rospy.loginfo(f"Deleted object: {object_name}")
-            
-    # except rospy.ServiceException as e:
-    #     rospy.logerr(f"Failed to delete object: {object_name}, error: {e}")
+    try:
+        get_world_properties = rospy.ServiceProxy('/gazebo/get_world_properties', GetWorldProperties)
+        delete_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
 
-          
+        # Get the list of models in the Gazebo environment
+        model_names = get_world_properties().model_names
+
+        if object_name in model_names:
+            # Object exists, proceed with deletion
+            req = DeleteModelRequest()
+            req.model_name = object_name
+            resp = delete_model(req)
+            rospy.loginfo(f"Deleted object: {object_name}")
+            # if object_name in target_positions:
+            #     del target_positions[object_name]
+            return True
+        else:
+            rospy.logwarn(f"Object '{object_name}' not found in the environment, skipping deletion.")
+            return False
+    except rospy.ServiceException as e:
+        rospy.logerr(f"Service call failed: {e}")
+
+            
